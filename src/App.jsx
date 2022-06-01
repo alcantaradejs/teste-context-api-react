d4565aa2ab4036978150a6d42bdb4b2832d178ec
Isaque Alcantara <alcantara.isaque167@gmail.com>
2022-06-01 13:44:47 +0000

    Implement:count-mirror

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import { Count } from "./components/count"
+import { Mirror } from "./components/mirror"
+
 function App() {
   return (
-    <div className="App">
-      <button> hello </button>
+    <div className="h-screen flex flex-col justify-center items-center">
+      <Count/>
+      <Mirror/>
     </div>
   )
 }
